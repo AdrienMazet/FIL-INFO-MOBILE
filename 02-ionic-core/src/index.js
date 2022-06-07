91a51d9b64c8f5bc22ca4a2b765f9db402dad636
Adrien Mazet <adrien.mazet.56@gmail.com>
2022-06-07 10:12:09 +0200

    TP2 : done

--- 02-ionic-core/src/index.js
+++ 02-ionic-core/src/index.js
@@ -8,7 +8,26 @@ const init = async () => {
   // la démarche n'est pas optimale car nous importons tous les composants
   await defineCustomElements();
 
-  // TODO
+  const response = await fetch(
+    "https://devfest-nantes-2018-api.cleverapps.io/blog"
+  );
+  const data = await response.json();
+  const list = document.getElementById("myList");
+  data.forEach((blogPost) => {
+    const { title, brief, image } = blogPost;
+    const card =
+      "<ion-card>" +
+      "<img " +
+      "src=" +
+      `https://devfest2018.gdgnantes.com${image} ` +
+      "/>" +
+      "<ion-card-header>" +
+      `<ion-card-title>${title}</ion-card-title>` +
+      "</ion-card-header>" +
+      `<ion-card-content>${brief}</ion-card-content>` +
+      "</ion-card>";
+    list.innerHTML += card;
+  });
 };
 
 init();
