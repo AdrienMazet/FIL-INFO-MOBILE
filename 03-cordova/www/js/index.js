9ec74f5ac8a59e1334d314e77dc4a228c47ab563
Apostophe <christophe.dsneto@gmail.com>
2022-06-07 18:34:34 +0200

    finished TP3

--- 03-cordova/www/js/index.js
+++ 03-cordova/www/js/index.js
@@ -15,15 +15,73 @@
  * KIND, either express or implied.  See the License for the
  * specific language governing permissions and limitations
  * under the License.
+*/
+
+/**
+ * DOM Manipulations
  */
+ const createListElement = (title, content) => {
+    const element = document.createElement('li');
+    element.innerHTML = `${title}: ${content}`;
+
+    return element;
+};
+
+const addDeviceInfo = () => {
+    const devicePropertiesList = document.createElement('ul');
+    
+    devicePropertiesList.appendChild(createListElement('Cordova', device.cordova));
+    devicePropertiesList.appendChild(createListElement('Model', device.model));
+    devicePropertiesList.appendChild(createListElement('Uuid', device.uuid));
+
+    document.getElementById('deviceinfo').append(devicePropertiesList);
+};
+
+const cleanNetworkInfo = () => {
+    const networkInfo = document.getElementById('networkinfo');
+    networkInfo.innerHTML = '';
+};
+
+const addNetworkInfo = () => {
+    const connection = navigator.connection;
+    const networkStatus = connection.type;
+
+    const networkPropertiesList = document.createElement('ul');
+
+    networkPropertiesList.appendChild(createListElement('Network status', networkStatus));
+
+    document.getElementById('networkinfo').append(networkPropertiesList);
+};
+
+const updateNetworkStatus = () => {
+    cleanNetworkInfo();
+
+    addNetworkInfo();
+};
 
 // Wait for the deviceready event before using any of Cordova's device APIs.
 // See https://cordova.apache.org/docs/en/latest/cordova/events/events.html#deviceready
 document.addEventListener('deviceready', onDeviceReady, false);
 
 function onDeviceReady() {
-    // Cordova is now initialized. Have fun!
-
     console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
     document.getElementById('deviceready').classList.add('ready');
+
+    addDeviceInfo();
+
+    addNetworkInfo();
 }
+
+/**
+ * Add a listener for the Online and Offline events.
+ */
+document.addEventListener("online", onOnline, false);
+document.addEventListener("offline", onOffline, false);
+
+const onOnline = () => {
+    updateNetworkStatus();
+};
+
+const onOffline = () => {
+    updateNetworkStatus();
+};
